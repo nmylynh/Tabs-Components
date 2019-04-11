93872a9f5c1ff41daa9e9bf098267d3cfe86ab9e
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-11 15:15:47 -0700

    fixed bracket

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -12,10 +12,11 @@ class Dropdown {
     // Add a click handler to the button reference and call the toggleContent method.
     this.button.addEventListener("click", this.toggleContent()); 
 
-  toggleContent() {
+   toggleContent(){
     
     // Toggle the ".dropdown-hidden" class off and on
     this.content.classList.toggle('dropdown-hidden');
+    }
   }
 }
 
--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -12,7 +12,9 @@ class TabLink {
     // Using the custom data attribute get the associated Item element
     // this.itemElement;
 
-    this.itemElement = document.querySelector(`.tabs-item[data-tab="${this.data}"]`);
+    // var element = document.body.querySelector('.element[data-id="123456789"]')
+
+    this.itemElement = document.querySelector('.tabs-item[data-tab = "${this.data}"]');
     
     // Using the Item element, create a new instance of the TabItem class
     // this.tabItem;
@@ -30,8 +32,8 @@ class TabLink {
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
     // Array.from(links).forEach();
 
-    links.forEach(function(links){
-      links.classList.remove("tabs-link-selected")
+    Array.from(links).forEach(function(links){
+      Array.from(links).remove("tabs-link-selected")
     });
 
     // Add a class named "tabs-link-selected" to this link
@@ -54,6 +56,7 @@ class TabItem {
   select() {
     // Select all ".tabs-item" elements from the DOM
     // const items;
+    const items 
 
     // Remove the class "tabs-item-selected" from each element
     
